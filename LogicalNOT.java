60e3cecc09a14f3e1b97d407743dea8c84ddcaf7
Pawankumar Chidurala <chpaone09@hotmail.com>
2020-10-19 03:11:37 +0530

    Program to demonstrate Logical NOT ( ! ) Operator in java

---
+++ LogicalNOT.java
@@ -0,0 +1,20 @@
+/*
+***********************************************************
+
+Topic   : Program to demonstrate Logical NOT ( ! ) Operator in java.
+Author  : Chpaone09®
+Date    : Oct 19, 2020
+
+************************************************************
+*/
+
+public class LogicalNOT {
+    public static void main(String[] args) {
+        boolean a = true;
+
+        System.out.print("\n\n\t Original Value of A >> "+ a);
+
+        System.out.print("\n\n\t After applying Logical NOT Operator on A >> "+ !a);
+
+    }
+}
